d334a0bce2b9087c8facdfafc5428a9eff8dc229
Mateus <mateus_b09@hotmail.com>
2024-02-05 13:27:35 -0300

    yessssss

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,17 @@
 from data.dumb_import import dumb_import_function
 
 
+def __get_last_segment(event):
+    path = event['path']  # Get the path from the event
+    path_segments = path.split('/')  # Split the path into segments
+    return path_segments[-1]
+
+
 def lambda_handler(event, context):
+    last_segment = __get_last_segment(event)
     return {
         'statusCode': 200,
-        'body': dumb_import_function(),
+        'body': last_segment + dumb_import_function(),
         'headers': {
             'Content-Type': 'text/plain'
         }
