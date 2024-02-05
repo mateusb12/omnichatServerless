d7cf203fc9df10e30b58a2b00b06e72622186607
Mateus <mateus_b09@hotmail.com>
2024-02-05 12:29:07 -0300

    test aws 3!

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,6 @@
+from utils.corsBlocker import createResponseWithAntiCorsHeaders
+
+
 def lambda_handler(event, context):
     print("Hello, World!")
+    return createResponseWithAntiCorsHeaders("Hello from Python!", 200)
