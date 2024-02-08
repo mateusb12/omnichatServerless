2eb4cfb9c9386eab541f7c0e700c7e05a9ae49df
Mateus <mateus_b09@hotmail.com>
2024-02-08 18:04:42 -0300

    handler update

--- app.py
+++ app.py
@@ -25,10 +25,20 @@ def handler(event, context):
     headers = event["headers"] if "headers" in event else {}
     http_method = event['requestContext']['http']['method']
     path = event["requestContext"]["http"]["path"].split("/")
+    main_suffix = path[0]
+    secondary_suffix = path[1] if len(path) > 1 else None
+    available_operations = ["conversation_handler"]
+    if main_suffix not in available_operations:
+        return __get_standard_error_message(f'Invalid operation /{main_suffix}.'
+                                            f' Only available operations are {available_operations}')
+    return conversation_handler(path_list=path, http_method=http_method, event=event)
+
+
+def conversation_handler(path_list: List[str], http_method: str, event: dict):
     available_operations = ["get_all_conversations", "update_conversation", "update_multiple_conversations"]
-    suffix = path[-1]
+    suffix = path_list[-1]
     if suffix not in available_operations:
-        return __get_invalid_method_error_message(path, available_operations)
+        return __get_invalid_method_error_message(path_list, available_operations)
     operation_dict = {
         "get_all_conversations": ("GET", get_all_conversations),
         "update_conversation": ("PUT", update_conversation),
@@ -42,22 +52,3 @@ def handler(event, context):
 
     return {"headers": {"Content-Type": "application/json"}, "statusCode": response_code,
             "body": json.dumps(content) if content else json.dumps(reason)}
-
-# def lambda_handler(event, context) -> dict:
-#     headers = event["headers"]
-#     http_method = event['requestContext']['http']['method']
-#     path = event["requestContext"]["http"]["path"].split("/")
-#     available_operations = ["get_all_conversations", "update_conversation", "update_multiple_conversations"]
-#     suffix = path[-1]
-#     if suffix not in available_operations:
-#         return __get_invalid_method_error_message(path, available_operations)
-#     operation_dict = {
-#         "get_all_conversations": ("GET", get_all_conversations),
-#         "update_conversation": ("PUT", update_conversation),
-#         "update_multiple_conversations": ("PUT", update_multiple_conversations)
-#     }
-#     required_method, operation_func = operation_dict[suffix]
-#     if required_method != http_method:
-#         return __get_standard_error_message(f'Only {required_method} requests are accepted for the operation {suffix}')
-#     result = operation_func()
-#     return {"statusCode": 200, "headers": {"Content-Type": "application/json"}, "body": json.dumps(result)}
